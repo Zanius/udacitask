f6d27105b29e0efcbad45edda3be8ffb9ff3cea2
Zane Brown <zib1991@gmail.com>
2016-02-28 13:56:55 -0500

    Put formate date methods inside of listable

--- lib/listable.rb
+++ lib/listable.rb
@@ -3,4 +3,15 @@ module Listable
   def format_description(description)
     "#{description}".ljust(30)
   end
+
+  def format_date(options = {})
+    @due = options[:due]
+    @due ? @due.strftime("%D") : "No due date"
+    dates = []
+    start_date = options[:start_date].strftime("%D")
+    end_date = options[:end_date].strftime("%D")
+    dates << start_date + "--"
+    dates << end_date
+    dates = "N/A" if !dates
+  end
 end
